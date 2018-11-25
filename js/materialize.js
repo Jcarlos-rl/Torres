2d52ebc1fd42ba69a12687a8901a7f465046a321
Jcarlos-rl <carlos_-619@hotmail.com>
2018-11-24 18:31:11 -0600

    Inicio de proyecto

---
+++ js/materialize.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $('.slider').slider({
+        indicators: false,
+        height: $(window).height(),
+        interval: 4500
+    });
+    $('#slider2').slider({
+        indicators: false,
+        height: 369,
+        interval: 4500
+    });
+});
+$(document).ready(function(){
+    $('.parallax').parallax();
+});
