7e7be79b3be8a01ac6dde532266416c8f5038f14
Jcarlos-rl <carlos_-619@hotmail.com>
2019-01-21 18:47:29 -0600

    Promocion 1.0

---
+++ mail.php
@@ -0,0 +1,11 @@
+<?php
+    $destino = "carlos_-619@hotmail.com";
+    $nombre = $_POST["nombre"];
+    $email = $_POST["email"];
+    $tel = $_POST["telefono"];
+    $mensaje = $_POST["mensaje"];
+    echo $nombre." ".$email." ".$tel." ".$mensaje;
+    $contenido = "Nombre: ".$nombre."\nE-mail: ".$email."\nTelefono:".$tel."\nMensaje: ".$mensaje;
+    mail($destino,"Contacto Torres Cruz del Sur",$contenido);
+    header("Location:index.html");
+?>
